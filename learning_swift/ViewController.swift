ac0e494db14a19c22196af329b24eacb8d538ddf
isao_e <isao0214@gmail.com>
2018-06-03 23:30:07 +0900

    add myTextField

--- learning_swift/ViewController.swift
+++ learning_swift/ViewController.swift
@@ -9,9 +9,12 @@
 import UIKit
 
 class ViewController: UIViewController {
+
     @IBAction func tapHandler(_ sender: Any) {
     }
-    
+
+    @IBOutlet weak var myTextField: UITextField!
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
