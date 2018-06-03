077c625014932904f1800d69b5ecdb8fa6c68416
isao_e <isao0214@gmail.com>
2018-06-03 16:54:18 +0900

    Initial Commit

---
+++ learning_swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  learning_swift
+//
+//  Created by isao_ebisujima on 2018/06/03.
+//  Copyright © 2018年 isao_ebisujima. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
